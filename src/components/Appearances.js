30ad2b138d9c02aa66f8dacae30212165c760313
Samuel Malcolm <samuelmalcolm@C02XFC5BJG5J.local>
2019-04-14 14:23:04 +1000

    added svgs

--- src/components/Appearances.js
+++ src/components/Appearances.js
@@ -4,31 +4,32 @@ import moment from 'moment';
 
 const Ticket = (props) => {
 	return (
-	<svg className="ticket" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-		 viewBox="0 0 449 447" >
+		<svg className="ticket" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 200 200" style={{"enable-background":"new 0 0 200 200"}}>
+			<g>
+				<path style={{'fill':props.highlight}} d="M182.89,53.15l-11.96-11.41c-2.19,2.3-5.13,3.46-8.08,3.46c-2.77,0-5.55-1.02-7.71-3.09c-4.46-4.26-4.62-11.33-0.37-15.79
+		L142.82,14.9c-2.03-1.94-4.64-2.9-7.24-2.9c-2.76,0-5.52,1.09-7.58,3.24L100.07,44.5L100,44.44l-3.41,3.71l-79.46,83.24
+		c-4,4.19-3.84,10.82,0.34,14.82l40.07,38.25c2.03,1.94,4.64,2.9,7.24,2.9c2.76,0,5.52-1.09,7.58-3.24L183.23,67.97
+		C187.23,63.78,187.08,57.14,182.89,53.15z M177.45,62.44L66.58,178.59c-0.64,0.67-1.4,0.77-1.8,0.77c-0.45,0-1.12-0.12-1.71-0.69
+		l-40.07-38.25c-0.61-0.59-0.76-1.27-0.77-1.74c-0.01-0.47,0.1-1.16,0.69-1.77L133.79,20.77c0.64-0.67,1.4-0.77,1.8-0.77
+		c0.45,0,1.12,0.12,1.71,0.69l7.44,7.1c-2.35,6.86-0.69,14.77,4.89,20.1c3.58,3.42,8.28,5.3,13.23,5.3c2.44,0,4.84-0.47,7.07-1.36
+		l7.44,7.1c0.61,0.59,0.76,1.27,0.77,1.74C178.14,61.14,178.03,61.83,177.45,62.44z"/>
+				<rect style={{'fill':props.highlight}} x="107.16" y="55.5" transform="matrix(0.6761 -0.7368 0.7368 0.6761 -8.2851 101.3739)" width="8" height="9.21"/>
+				<rect style={{'fill':props.highlight}} x="120.74" y="67.96" transform="matrix(0.6762 -0.7367 0.7367 0.6762 -13.0755 115.394)" width="8" height="9.21"/>
+				<rect style={{'fill':props.highlight}} x="134.31" y="80.43" transform="matrix(0.6762 -0.7367 0.7367 0.6762 -17.8597 129.4315)" width="8" height="9.21"/>
+			</g>
+		</svg>
+	)
+}
 
-	<g>
-		<path className="ticketPath" style={{'fill':props.highlight, 'strokeWidth':13}} d="M301.83,43.92c2.81,0,5.48,1.07,7.51,3.01l19.46,18.58c-2.27,5.12-3.4,10.72-3.27,16.48c0.24,10.26,4.46,19.82,11.89,26.91
-			c7.18,6.86,16.61,10.63,26.54,10.63c6,0,11.89-1.43,17.18-4.07l19.49,18.6c4.34,4.14,4.5,11.04,0.36,15.38L148.41,414.07
-			c-2.07,2.17-4.87,3.37-7.87,3.37c-2.81,0-5.48-1.07-7.51-3.01l-91.29-87.14c-4.34-4.14-4.5-11.04-0.36-15.38L293.96,47.29
-			C296.03,45.12,298.83,43.92,301.83,43.92L301.83,43.92 M301.83,30.92c-6.3,0-12.59,2.48-17.28,7.39L31.98,302.93
-			c-9.11,9.54-8.75,24.66,0.79,33.76l91.29,87.14c4.62,4.41,10.56,6.61,16.49,6.61c6.3,0,12.59-2.48,17.28-7.39l252.58-264.61
-			c9.11-9.54,8.75-24.66-0.79-33.76l-27.24-26.01c-5,5.24-11.7,7.87-18.4,7.87c-6.31,0-12.63-2.33-17.56-7.04
-			c-10.16-9.7-10.54-25.8-0.84-35.96l-27.24-26.01C313.69,33.11,307.76,30.92,301.83,30.92L301.83,30.92z"/>
-	</g>
-	<g>
-		<line className="st0" x1="339.65" y1="226.31" x2="214.6" y2="111.54"/>
-		<g>
-			<line style={{'fill':props.highlight, 'strokeWidth':13}} className="st1" x1="339.65" y1="226.31" x2="335.23" y2="222.25"/>
-			<line style={{'fill':props.highlight, 'strokeWidth':13}} className="st2" x1="326.29" y1="214.05" x2="223.49" y2="119.7"/>
-			<line style={{'fill':props.highlight, 'strokeWidth':13}} className="st1" x1="219.02" y1="115.59" x2="214.6" y2="111.54"/>
-		</g>
-	</g>
+const Information = (props) => {
+	return (
+<svg className="information" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+	 viewBox="0 0 200 200" style={{"enableBackground":"new 0 0 200 200"}}>
+<circle style={{"fill":"none","stroke":props.highlight}} className="st0" cx="99.95" cy="99.75" r="81.05"/>
+<text style={{ 'fill':props.highlight }} transform="matrix(1 0 0 1 84.4004 146.457)" className="st1 st2">i</text>
+</svg>
 
-	</svg>
-	
-
-)
+	)
 }
 
 export default function Appearances(props) {
@@ -54,7 +55,7 @@ export default function Appearances(props) {
 				{appearances.map((appearance) => {
 					return (
 						<tr>
-							<td>{moment(appearance.date).format('lll')}</td>
+							<td>{moment(appearance.date).format('ll')}</td>
 							<td>{appearance.event}</td>
 							<td>{appearance.name}</td>
 							<td>{appearance.location}</td>
@@ -70,7 +71,7 @@ export default function Appearances(props) {
 							<td style={{ 'textAlign': 'center' }}>
 								{(appearance.info) ?
 									<a target="_blank" href={appearance.info}>
-										info
+										<Information highlight={props.highlight} />
 									</a>
 
 									: <td></td>}
@@ -89,6 +90,6 @@ export default function Appearances(props) {
 				})}
 			</tbody>
 
-		</table>
+		</table> 
 	)
 }
