0c22355c08ef6a681942dccd5b3e2fb56b6ff6c9
SamMalcolm <sam_malcolm@live.com.au>
2019-05-09 12:31:16 +1000

    added webpack to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
     entry: "./src/index.js",
     output: {
@@ -32,5 +32,5 @@ module.exports = {
             },
             { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
-    }
+    },
 }
