81140016fdf762aeaf44b7b8de4fc22a44747c70
SamMalcolm <sam_malcolm@live.com.au>
2019-05-14 08:48:17 +1000

    Trying to fix the album page

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -4,14 +4,24 @@ import axios from 'axios';
 
 const Song = (props) => {
     return (
-        <div className={(props.active) ? "songContainer" : "songContainer colourTransition"}>
-
-            <div onClick={() => { props.handleClick(props.name); }} style={{ 'backgroundColor': props.color, "zIndex": (props.active) ? "299" : "301" }} className="songTitle">
-                <h1 className={(props.active) ? "selected" : null}>{(props.active) ? "LOADING" : props.name}</h1>
-            </div>
-            {(props.active || props.clickCount) ? <iframe src={props.src} width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> : null}
-
+        <div>
+            {(props.active) ?
+                <div className="songContainer">
+                    <div onClick={() => { props.handleClick(props.name); }} style={{ 'backgroundColor': props.color, "zIndex": "299" }} className="songTitle">
+                        <h1 className="selected">LOADING</h1>
+                    </div>
+                    <iframe src={props.src} width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                </div>
+                :
+                <div className="songContainer">
+                    <div onClick={() => { props.handleClick(props.name); }} style={{ 'backgroundColor': props.color, "zIndex": "301" }} className="songTitle">
+                        <h1>{props.name}</h1>
+                    </div>
+                    {(props.clickCount) ? <iframe src={props.src} width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> : null}
+                </div>
+            }
         </div>
+
     )
 }
 
@@ -19,16 +29,21 @@ const Album = (props) => {
     const [songs, setSongs] = useState(props.songs);
 
     const handleClick = (name) => {
+        console.log("CLICK!");
         for (var key in songs) {
             songs[key].active = 0;
         }
+        console.log("After loop");
         let activeSong = songs.filter((song) => {
             return song.name == name;
         });
+        console.log("after filter");
         let index = songs.indexOf(activeSong[0]);
         songs[index].active = 1;
         songs[index].clickCount++;
+        console.log("Setting params");
         setSongs(songs);
+        console.log("Setting state");
     }
 
     return (
