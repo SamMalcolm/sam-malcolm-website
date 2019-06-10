dae1f6ba7785eca0fe23b7bc7d5a87b75dbbd05b
SamMalcolm <sam_malcolm@live.com.au>
2019-06-10 16:04:18 +1000

    added more files

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV != "prod") {
+    app.use(express.static(path.join(__dirname, 'public')));
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
