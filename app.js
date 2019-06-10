2197fdbe16748471b639bf1063d63ff750282032
SamMalcolm <sam_malcolm@live.com.au>
2019-06-10 16:23:22 +1000

    update

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-if (process.env.NODE_ENV != "prod") {
-    app.use(express.static(path.join(__dirname, 'public')));
-}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
