61459442b36131191a0646aa815e3bf12b9b32e2
SamMalcolm <sam_malcolm@live.com.au>
2019-07-02 07:46:52 +1000

    lynda page working in react

--- src/components/YoutubePlaylist.js
+++ src/components/YoutubePlaylist.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios';
 
-const ytpMenuItem = (data) => {
+const YtpMenuItem = (data) => {
 	return (
-		<div>
+		<div onClick={() => {
+			data.handleClick(data.index)
+		}} data-id={data.index}>
 			<img src={data.src} />
-			<h3>{data.title}</h3>
-			<i>{data.duration}</i>
+			<span>
+				<h4>{data.title}</h4>
+				<i>{data.duration}</i>
+			</span>
 		</div>
 	)
 }
@@ -15,26 +19,38 @@ export default function YoutubePlaylist() {
 
 	const [autoplay, setAutoplay] = useState(true);
 	const [playlist, setPlaylist] = useState([]);
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
 	const [transcript, setTranscript] = useState([]);
 	const [resources, setResources] = useState([]);
 	const [channel, setChannel] = useState([]);
+
+	const handleClick = (index) => {
+		// console.log(e);
+		// let index = e.target.getAttribute("data-id");
+		// console.log(e.target);
+		console.log("INDEX " + index);
+		console.log("VIDEO TITLE " + playlist[index].snippet.title)
+		setTitle(playlist[index].snippet.title);
+		setDescription(playlist[index].snippet.description);
+		player.loadVideoById(playlist[index].contentDetails.videoId);
+
+	}
 
 	useEffect(() => {
 		Axios.get('/api/youtube/PLT_xscTFmzgrH4D4BnIa6dru_iLZRCYDR').then((response) => {
 			setPlaylist(response.data.items);
 			setChannel(response.data.channel);
 
-			const onPlayerReady = () => {
+			const onPlayerReady = () => { }
 
-			}
-
-			const onPlayerStateChange = () => {
-
-			}
+			const onPlayerStateChange = () => { }
 
 			window.onYouTubeIframeAPIReady = () => {
 				console.log("IFRAME API READY");
+
 				player = new YT.Player('player', {
+
 					height: '390',
 					width: '640',
 					videoId: response.data.items[0].contentDetails.videoId,
@@ -43,55 +59,72 @@ export default function YoutubePlaylist() {
 						'onStateChange': onPlayerStateChange
 					}
 				});
+
+				setTitle(response.data.items[0].snippet.title);
+				setDescription(response.data.items[0].snippet.description);
 			}
 
 			var tag = document.createElement('script');
 			tag.src = "https://www.youtube.com/iframe_api";
 			var firstScriptTag = document.getElementsByTagName('script')[0];
 			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 		})
 	}, [])
 
 	return (
 		<div className="ytp_container">
 			<div className="ytp_header">
-				<h1>Video Title</h1>
+				<h1>{title}</h1>
 				{(typeof channel != "undefined" && typeof channel.items != "undefined") &&
 					<div>
 						<img src={channel.items[0].snippet.thumbnails.default.url} />
-						<h5>{channel.items[0].snippet.title}</h5>
+						<h4>{channel.items[0].snippet.title}</h4>
 					</div>
 				}
+
 			</div>
 			<div className="ytp_video_theatre_container">
 				<div className="ytp_video_theatre">
-					<div className="ytp_player" id="player">
-
+					<div className="ytp_sub_container">
+						<div className="ytp_player" id="player">
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className="ytp_meta_container" >
+			<div className="ytp_meta_container">
 				<div className="ytp_video_menu">
-					<h3>Video Menu</h3>
-					{(playlist).map((item) => {
-						console.log("mapping playlist object");
-						console.log(item);
-						(
-							<ytpMenuItem title={item.snippet.title} src={item.snippet.thumbnails.default.url} duration="1s" />
-						)
-					})}
+					<h3>Video Menu</h3> {
+						(playlist).map((item, i) => {
+							return (
+								<YtpMenuItem index={i} handleClick={handleClick} title={item.snippet.title} src={item.snippet.thumbnails.default.url} duration="1s" />
+							)
+						})
+					}
+
 				</div>
 				<div className="ytp_meta_data">
-					<h3>Video Transcript</h3>
-					<span className="ytp_transcript"></span>
-					<h3>Description</h3>
-					<span className="ytp_description"></span>
-					<h3>Additional Resources</h3>
-					<span className="ytp_resources"></span>
+					{(transcript) &&
+						<div>
+							<h3>Video Transcript</h3>
+							<span className="ytp_transcript"></span>
+						</div>
+					}
+
+					{(description) &&
+						<div>
+							<h3>Description</h3>
+							<span className="ytp_description"></span>
+						</div>
+					}
+					{(resources) &&
+						<div>
+							<h3>Resources</h3>
+							<span className="ytp_resources"></span>
+						</div>
+					}
+
 				</div>
 			</div>
-
 		</div>
 	)
 }
