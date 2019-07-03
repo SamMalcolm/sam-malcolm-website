50bfd37f222fc78b73fc0cadd915f2a7146fa5a7
SamMalcolm <sam_malcolm@live.com.au>
2019-07-04 07:45:14 +1000

    page working, only transcript left

--- src/components/YoutubePlaylist.js
+++ src/components/YoutubePlaylist.js
@@ -3,10 +3,12 @@ import Axios from 'axios';
 
 const YtpMenuItem = (data) => {
 	return (
-		<div onClick={() => {
+		<div className="ytp_menu_item" onClick={() => {
 			data.handleClick(data.index)
 		}} data-id={data.index}>
-			<img src={data.src} />
+			<div className="ytp_menu_img">
+				<img src={data.src} />
+			</div>
 			<span>
 				<h4>{data.title}</h4>
 				<i>{data.duration}</i>
@@ -18,6 +20,7 @@ const YtpMenuItem = (data) => {
 export default function YoutubePlaylist(data) {
 
 	const [autoplay, setAutoplay] = useState(true);
+	const [darkTheme, setDarkTheme] = useState(true);
 	const [playlist, setPlaylist] = useState([]);
 	const [title, setTitle] = useState("");
 	const [description, setDescription] = useState("");
@@ -64,71 +67,80 @@ export default function YoutubePlaylist(data) {
 		Axios.get('/api/tutorial/' + data.id).then((response) => {
 			setPlaylist(response.data.items);
 			setChannel(response.data.channel);
-			var tag = document.createElement('script');
-			tag.src = "https://www.youtube.com/iframe_api";
-			var firstScriptTag = document.getElementsByTagName('script')[0];
-			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			if (!document.querySelector("script[src=\"https://www.youtube.com/iframe_api\"]")) {
+				var tag = document.createElement('script');
+				tag.src = "https://www.youtube.com/iframe_api";
+				var firstScriptTag = document.getElementsByTagName('script')[0];
+				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			} else {
+				onYouTubeIframeAPIReady();
+			}
 		})
 	}, [])
 
 	return (
-		<div className="ytp_container">
-			<div className="ytp_header">
-				<h1>{title}</h1>
-				{(typeof channel != "undefined" && typeof channel.items != "undefined") &&
-					<div>
-						<img src={channel.items[0].snippet.thumbnails.default.url} />
-						<h4>{channel.items[0].snippet.title}</h4>
+		<div className={(darkTheme) ? "theme-dark" : "theme-light"} >
+			<div className="ytp_container">
+				<div className="ytp_header">
+					<h1>{title}</h1>
+					{(typeof channel != "undefined" && typeof channel.items != "undefined") &&
+						<div>
+							<img src={channel.items[0].snippet.thumbnails.default.url} />
+							<h4>{channel.items[0].snippet.title}</h4>
+						</div>
+					}
+					<div className="ytp_theme_switch">
+						<span>Dark Theme: </span>
+						<input onChange={e => { setDarkTheme(e.target.checked) }} type="checkbox" checked={(darkTheme) ? "true" : "false"} />
 					</div>
-				}
-
-			</div>
-			<div className="ytp_video_theatre_container">
-				<div className="ytp_video_theatre">
-					<div className="ytp_sub_container">
-						<div className="ytp_player" id="player">
+				</div>
+				<div className="ytp_video_theatre_container">
+					<div className="ytp_video_theatre">
+						<div className="ytp_sub_container">
+							<div className="ytp_player" id="player">
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div className="ytp_meta_container">
-				<div className="ytp_video_menu">
-					<h3>Video Menu</h3> {
-						(playlist).map((item, i) => {
-							return (
-								<YtpMenuItem index={i} handleClick={playVideo} title={item.snippet.title} src={item.snippet.thumbnails.default.url} duration="1s" />
-							)
-						})
-					}
-				</div>
-				<div className="ytp_meta_data">
-					<p>Autoplay</p>
-					<div className="switch-container position-relative form-group">
-						<label className="switch">
-							<input checked={autoplay} onChange={(e) => { setAutoplay(e.target.checked) }} type="checkbox" className="form-check-input" />
-							<span className="slider round"></span>
-						</label>
+				<div className="ytp_meta_container">
+					<div className="ytp_video_menu">
+						<h3>Video Menu</h3> {
+							(playlist).map((item, i) => {
+								return (
+									<YtpMenuItem index={i} handleClick={playVideo} title={item.snippet.title} src={item.snippet.thumbnails.default.url} duration="1s" />
+								)
+							})
+						}
 					</div>
-					{(transcript) &&
-						<div>
-							<h3>Video Transcript</h3>
-							<span className="ytp_transcript"></span>
+					<div className="ytp_meta_data">
+						<p>Autoplay</p>
+						<div className="switch-container position-relative form-group">
+							<label className="switch">
+								<input checked={autoplay} onChange={(e) => { setAutoplay(e.target.checked) }} type="checkbox" className="form-check-input" />
+								<span className="slider round"></span>
+							</label>
 						</div>
-					}
+						{(transcript) &&
+							<div>
+								<h3>Video Transcript</h3>
+								<span className="ytp_transcript"></span>
+							</div>
+						}
 
-					{(description) &&
-						<div>
-							<h3>Description</h3>
-							<span className="ytp_description"></span>
-						</div>
-					}
-					{(resources) &&
-						<div>
-							<h3>Resources</h3>
-							<span className="ytp_resources"></span>
-						</div>
-					}
+						{(description) &&
+							<div>
+								<h3>Description</h3>
+								<span className="ytp_description"></span>
+							</div>
+						}
+						{(resources) &&
+							<div>
+								<h3>Resources</h3>
+								<span className="ytp_resources"></span>
+							</div>
+						}
 
+					</div>
 				</div>
 			</div>
 		</div>
