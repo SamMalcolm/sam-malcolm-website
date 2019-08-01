2a11e21fa3ee8ac3be6aaaa5ce30d0109fed2816
SamMalcolm <sam_malcolm@live.com.au>
2019-08-02 08:33:45 +1000

    CRD working just need to add the U

--- src/admin/components/CRUDBlog.js
+++ src/admin/components/CRUDBlog.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios';
 import moment from 'moment';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 export default function CRUDBlog() {
 	const [posts, setPosts] = useState([]);
@@ -13,8 +15,14 @@ export default function CRUDBlog() {
 
 	const handleDelete = (e) => {
 		if (confirm('Are you sure you want to delete this post?')) {
-			console.log("DELETING " + e.target.getAttribute("data-id"))
-			Axios.delete("/api/blog/" + e.target.getAttribute("data-id"))
+			let deleteId = e.target.getAttribute("data-id")
+			Axios.delete("/api/blog/" + deleteId).then((resp) => {
+				var newPosts = posts.map((post) => { return post; });
+				var deletedIndex = newPosts.filter((post) => { return post._id == deleteId });
+				deletedIndex = newPosts.indexOf(deletedIndex[0]);
+				newPosts.splice(deletedIndex, 1);
+				setPosts(newPosts);
+			})
 		}
 	}
 
@@ -53,7 +61,7 @@ export default function CRUDBlog() {
 								<tr>
 									<td>{post.title}</td>
 									<td>{moment(post.date).format("lll")}</td>
-									<td><button>EDIT</button></td>
+									<td><button className="btn btn-secondary">EDIT</button></td>
 									<td><button class="btn btn-danger" data-id={post._id} onClick={handleDelete}>DELETE</button></td>
 								</tr>
 							)
@@ -62,6 +70,20 @@ export default function CRUDBlog() {
 				</table>
 
 			</div>
+			<Modal.Dialog>
+				<Modal.Header closeButton>
+					<Modal.Title>Modal title</Modal.Title>
+				</Modal.Header>
+
+				<Modal.Body>
+					<p>Modal body text goes here.</p>
+				</Modal.Body>
+
+				<Modal.Footer>
+					<Button variant="secondary">Close</Button>
+					<Button variant="primary">Save changes</Button>
+				</Modal.Footer>
+			</Modal.Dialog>
 		</div>
 	)
 }
