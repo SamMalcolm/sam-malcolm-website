e86d49e3b847e1d660e5e90e1831fb31f9fa3661
SamMalcolm <sam_malcolm@live.com.au>
2020-01-11 15:03:36 +1100

    added sensitive info

---
+++ keys/google_api.js
@@ -0,0 +1,2 @@
+exports.googleApiKey = "AIzaSyD1V1_OSkPd10g1FuyBN7hzccuLzN_ioVQ";
+
---
+++ keys/nodemailer_transport.js
@@ -0,0 +1,11 @@
+let transport = {
+	host: "smtp.gmail.com",
+	secure: false,
+	port: 587,
+	auth: {
+		user: 'sam.malcolm.media@gmail.com',
+		pass: 'LedZep123!@#'
+	}
+}
+
+module.exports = transport;
