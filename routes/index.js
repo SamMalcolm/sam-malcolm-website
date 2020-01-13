6de5133cc7926896ade264d012f681f7e59c0e38
SamMalcolm <sam_malcolm@live.com.au>
2020-01-13 19:46:11 +1100

    various fixes

--- routes/index.js
+++ routes/index.js
@@ -4,134 +4,164 @@ var mongoose = require('mongoose');
 var workModel = require('../models/work');
 const blogModel = require('../models/blog');
 const config = require('config');
-
+const bucket = (typeof process.env.S3_BUCKET != "undefined") ? process.env.S3_BUCKET : false;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let path = "Sam Malcolm Media";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media"
-  meta.feature_image = "assets/ui_images/social_share.png"
-  meta.social_description = "The official website for Sam Malcolm, the Melbourne based Digital Media Designer"
-  meta.feature_image_alt = "An image of Sam Malcolms logo, the outline of a cube with a gradient coloured background"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media"
+	meta.feature_image = "assets/ui_images/social_share.png"
+	meta.social_description = "The official website for Sam Malcolm, the Melbourne based Digital Media Designer"
+	meta.feature_image_alt = "An image of Sam Malcolms logo, the outline of a cube with a gradient coloured background"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 router.get('/snooker', function (req, res, next) {
-  let path = "Sam Malcolm Media | Snooker";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | Snooker"
-  meta.feature_image = "assets/ui_images/snooker2.jpg"
-  meta.social_description = "Summary page of Sam Malcolms Snooker & Billiards results"
-  meta.feature_image_alt = "An image of Sam Malcolm playing snooker, with the rest"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | Snooker";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | Snooker"
+	meta.feature_image = "assets/ui_images/snooker2.jpg"
+	meta.social_description = "Summary page of Sam Malcolms Snooker & Billiards results"
+	meta.feature_image_alt = "An image of Sam Malcolm playing snooker, with the rest"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 router.get('/blog', function (req, res, next) {
-  let path = "Sam Malcolm Media | Blog";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | Blog"
-  meta.feature_image = "assets/ui_images/social_share.png"
-  meta.social_description = "Blog page of Sam Malcolm's official website"
-  meta.feature_image_alt = "An image of Sam Malcolms logo, the outline of a cube with a gradient coloured background"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | Blog";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | Blog"
+	meta.feature_image = "assets/ui_images/social_share.png"
+	meta.social_description = "Blog page of Sam Malcolm's official website"
+	meta.feature_image_alt = "An image of Sam Malcolms logo, the outline of a cube with a gradient coloured background"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 router.get('/film', function (req, res, next) {
-  let path = "Sam Malcolm Media | Film Criticism";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | Film Criticism"
-  meta.feature_image = "assets/ui_images/film.jpg"
-  meta.social_description = "A curated resource of Sam Malcolm's film criticism across various platforms"
-  meta.feature_image_alt = "An image a film clapperboard"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | Film Criticism";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | Film Criticism"
+	meta.feature_image = "assets/ui_images/film.jpg"
+	meta.social_description = "A curated resource of Sam Malcolm's film criticism across various platforms"
+	meta.feature_image_alt = "An image a film clapperboard"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 router.get('/film', function (req, res, next) {
-  let path = "Sam Malcolm Media | Film Criticism";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | Film Criticism"
-  meta.feature_image = "assets/ui_images/film.jpg"
-  meta.social_description = "A curated resource of Sam Malcolm's film criticism across various platforms"
-  meta.feature_image_alt = "An image of a film clapperboard"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | Film Criticism";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | Film Criticism"
+	meta.feature_image = "assets/ui_images/film.jpg"
+	meta.social_description = "A curated resource of Sam Malcolm's film criticism across various platforms"
+	meta.feature_image_alt = "An image of a film clapperboard"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 router.get('/about', function (req, res, next) {
-  let path = "Sam Malcolm Media | About";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | About"
-  meta.feature_image = "assets/profile/small_me_for_site.jpg"
-  meta.social_description = "The about me page of Sam Malcolm's official website"
-  meta.feature_image_alt = "An image of Sam Malcolm"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | About";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | About"
+	meta.feature_image = "assets/profile/small_me_for_site.jpg"
+	meta.social_description = "The about me page of Sam Malcolm's official website"
+	meta.feature_image_alt = "An image of Sam Malcolm"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 router.get('/contact', function (req, res, next) {
-  let path = "Sam Malcolm Media | Contact";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | Contact"
-  meta.feature_image = "assets/ui_images/social_share.png"
-  meta.social_description = "The contact page of Sam Malcolm's official website"
-  meta.feature_image_alt = "An image of Sam Malcolms logo, the outline of a cube with a gradient coloured background"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | Contact";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | Contact"
+	meta.feature_image = "assets/ui_images/social_share.png"
+	meta.social_description = "The contact page of Sam Malcolm's official website"
+	meta.feature_image_alt = "An image of Sam Malcolms logo, the outline of a cube with a gradient coloured background"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 router.get('/work', function (req, res, next) {
-  let path = "Sam Malcolm Media | Work";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | Work"
-  meta.feature_image = "assets/ui_images/social_share.png"
-  meta.social_description = "A curated list of folio works by Sam Malcolm"
-  meta.feature_image_alt = "An image of Sam Malcolms logo, the outline of a cube with a gradient coloured background"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | Work";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | Work"
+	meta.feature_image = "assets/ui_images/social_share.png"
+	meta.social_description = "A curated list of folio works by Sam Malcolm"
+	meta.feature_image_alt = "An image of Sam Malcolms logo, the outline of a cube with a gradient coloured background"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 router.get('/music', function (req, res, next) {
-  let path = "Sam Malcolm Media | Music";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | Music"
-  meta.feature_image = "assets/ui_images/music.jpg"
-  meta.social_description = "Sam Malcolm's discography and information about upcoming music projects"
-  meta.feature_image_alt = "An image of Sam Malcolm playing guitar"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | Music";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | Music"
+	meta.feature_image = "assets/ui_images/music.jpg"
+	meta.social_description = "Sam Malcolm's discography and information about upcoming music projects"
+	meta.feature_image_alt = "An image of Sam Malcolm playing guitar"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 router.get('/appearances', function (req, res, next) {
-  let path = "Sam Malcolm Media | Live";
-  let meta = {};
-  meta.social_title = "Sam Malcolm Media | Live"
-  meta.feature_image = "assets/profile/small_me_for_site.jpg"
-  meta.social_description = "List of events for Sam Malcolm"
-  meta.feature_image_alt = "An image of Sam Malcolm"
-  res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
+	let path = "Sam Malcolm Media | Live";
+	let meta = {};
+	meta.social_title = "Sam Malcolm Media | Live"
+	meta.feature_image = "assets/profile/small_me_for_site.jpg"
+	meta.social_description = "List of events for Sam Malcolm"
+	meta.feature_image_alt = "An image of Sam Malcolm"
+	res.render('index', { title: path, meta: meta, url: config.get("siteAddress") });
 });
 
 
 router.get('/work/:work_id', (req, res) => {
-  workModel.findById(req.params.work_id, (err, docs) => {
-    console.log(docs);
-    res.render('index', {
+	workModel.findById(req.params.work_id, (err, docs) => {
+		console.log(docs);
+		if (docs.active) {
+			console.log(bucket);
 
-      meta: docs,
-      title: docs.name,
-      url: config.get("siteAddress")
-    })
-  })
+			if (bucket) {
+				if (docs.type == 2) {
+					docs.data = docs.data.map((item) => {
+						item.photo = bucket + item.photo;
+						item.thumbnail = bucket + item.thumbnail;
+						return item;
+					})
+				}
+				docs.feature_image = (docs.feature_image) ? bucket + docs.feature_image : "";
+				docs.thumb_src = (docs.thumb_src) ? bucket + docs.thumb_src : "";
+			} else {
+				docs.feature_image = (docs.feature_image) ? config.get("siteAddress") + docs.feature_image : "";
+				docs.thumb_src = (docs.thumb_src) ? config.get("siteAddress") + docs.thumb_src : "";
+			}
+			res.render('index', {
+				meta: docs,
+				title: docs.name,
+				url: config.get("siteAddress")
+			})
+		} else {
+			res.redirect('/');
+		}
+
+	})
 })
 
 router.get('/blog/:blog_id', (req, res) => {
-  blogModel.findById(req.params.blog_id, (err, docs) => {
-    console.log(docs);
-    res.render('index', {
-
-      meta: docs,
-      title: docs.name,
-      url: config.get("siteAddress")
-    })
-  })
+	blogModel.findById(req.params.blog_id, (err, docs) => {
+		docs.feature_image = (bucket) ? bucket + docs.feature_image : config.get("siteAddress") + docs.feature_image;
+		if (bucket) {
+			let doc = docs.markup;
+			doc = new JSDOM(doc).window.document;
+			let images = doc.querySelectorAll("img");
+			for (let i = 0; i < images.length; i++) {
+				images[i].setAttribute("src", bucket + images[i].getAttribute("src"))
+			}
+			docs.markup = doc.querySelector("body").innerHTML;
+		}
+		res.render('index', {
+			meta: docs,
+			title: docs.name,
+			url: config.get("siteAddress")
+		})
+	})
 })
 
 router.get('/pod/rss', (req, res) => {
-  let xml_response = `<?xml version="1.0" encoding="utf-8"?>
+	let xml_response = `<?xml version="1.0" encoding="utf-8"?>
 <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:itunesu="http://www.itunesu.com/feed" version="2.0">
 <channel>
 <link>https://sammalcolmmedia.com/</link>
@@ -178,8 +208,8 @@ router.get('/pod/rss', (req, res) => {
 
 </channel>
 </rss>`
-  res.type('application/rss+xml');
-  res.send(xml_response);
+	res.type('application/rss+xml');
+	res.send(xml_response);
 })
 
 module.exports = router;
